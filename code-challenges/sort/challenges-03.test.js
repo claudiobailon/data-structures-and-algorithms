e68f15ffe51fd069996694484c3b540ef6f13316
claudiobailon <c.bailonschubert@gmail.com>
2020-07-08 20:25:44 -0700

    Challenge 4 passes

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -59,7 +59,10 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    return a.length > b.length ? 1 : -1;
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
