f1acd10b9be6be5bee57fc0b79584557d485c657
claudiobailon <c.bailonschubert@gmail.com>
2020-07-09 19:10:57 -0700

    Third Challenge passes

--- code-challenges/regular-expressions/challenges-04.test.js
+++ code-challenges/regular-expressions/challenges-04.test.js
@@ -52,9 +52,22 @@ Write a function named isCapitalized that takes in a string. This function shoul
 
 Return an array containing all the matches.
 ------------------------------------------------------------------------------------------------ */
+// describe('Testing challenge 3', () => {
+//   test('It should only return words that begin with a capital letter', () => {
+//     const capitalResult = isCapitalized('We only want to Return the Words that begin With a capital Letter');
 
+//     expect(capitalResult).toStrictEqual([ 'We', 'Return', 'Words', 'With', 'Letter' ]);
+//     expect(capitalResult.length).toStrictEqual(5);
+
+//     expect(isCapitalized('Given by our hand in the meadow that is called Runnymede, between Windsor and Staines, on the fifteenth day of June in the seventeenth year of our reign (i.e. 1215: the new regnal year began on 28 May).')).toStrictEqual(['Given', 'Runnymede', 'Windsor', 'Staines', 'June', 'May']);
+
+//     expect(isCapitalized('these words are all failures')).toStrictEqual([]);
+//   });
+// });
 const isCapitalized = (str) => {
-  // Solution code here...
+  let regex = /[A-Z]\w+/g;
+  let stringOfCaps = str.match(regex) ;
+  return stringOfCaps || [];
 };
 
 /* ------------------------------------------------------------------------------------------------
