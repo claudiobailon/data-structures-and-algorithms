bd108792c0bcfd756fc9800f5f555bff8ddd2302
claudiobailon <c.bailonschubert@gmail.com>
2020-09-27 21:45:18 -0700

    Initial file structure

---
+++ data-Structures/src/main/java/data/Structures/stacksandqueues/Stack.java
@@ -0,0 +1,18 @@
+package data.Structures.stacksandqueues;
+
+public class Stack {
+    public class Node{
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+            this.next = next;
+        }
+        @Override
+        public String toString(){
+            return String.format()
+        }
+    }
+
+}
---
+++ data-Structures/src/test/java/data/Structures/stacksandqueues/StackTest.java
@@ -0,0 +1,4 @@
+package data.Structures.stacksandqueues;
+
+public class StackTest {
+}
