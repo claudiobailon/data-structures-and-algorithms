8814b4da3f32c8434d0f452b6b782b093b4c1624
claudiobailon <c.bailonschubert@gmail.com>
2020-09-27 22:33:16 -0700

    Stack class and tests completed

--- data-Structures/src/main/java/data/Structures/stacksandqueues/Node.java
+++ data-Structures/src/main/java/data/Structures/stacksandqueues/Node.java
@@ -1,4 +1,15 @@
 package data.Structures.stacksandqueues;
 
 public class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = next;
+    }
+    @Override
+    public String toString() {
+        return String.format("{%d}", this.value);
+    }
 }
--- data-Structures/src/main/java/data/Structures/stacksandqueues/Stack.java
+++ data-Structures/src/main/java/data/Structures/stacksandqueues/Stack.java
@@ -1,18 +1,34 @@
 package data.Structures.stacksandqueues;
 
 public class Stack {
-    public class Node{
-        int value;
-        Node next;
+    Node top = null;
 
-        public Node(int value) {
-            this.value = value;
-            this.next = next;
-        }
-        @Override
-        public String toString(){
-            return String.format()
-        }
+    public void push(int value){
+        Node newNode = new Node(value);
+        newNode.next= top;
+        top = newNode;
+    }
+    public int pop() throws NullPointerException{
+        if(isEmpty()) throw new NullPointerException();
+        int popped = top.value;
+        top = top.next;
+        return popped;
+    }
+    public int peek() throws NullPointerException{
+        if(isEmpty()) throw new NullPointerException();
+        return top.value;
+    }
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    @Override
+    public String toString(){
+        return toString(top);
+    }
+    public String toString(Node node){
+        if(node == null) return "NULL";
+        return String.format("%s -> ", node.toString()) + toString(node.next);
     }
 
 }
