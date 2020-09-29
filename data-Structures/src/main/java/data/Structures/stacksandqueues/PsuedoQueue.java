8cb37b1d884addb9bb765de304a4948d632fb824
claudiobailon <c.bailonschubert@gmail.com>
2020-09-28 21:39:45 -0700

    Completed tests and updated README

--- data-Structures/src/main/java/data/Structures/stacksandqueues/PsuedoQueue.java
+++ data-Structures/src/main/java/data/Structures/stacksandqueues/PsuedoQueue.java
@@ -4,15 +4,27 @@ public class PsuedoQueue {
     Stack inputStack = new Stack();
     Stack outputStack = new Stack();
 
-    public void enqueue(int value){
+    public void enqueue(int value) {
         inputStack.push(value);//pushes value to the top of the inputStack
     }
 
-    public int dequeue() throws NullPointerException{
-        if (inputStack == null && outputStack == null)throw new NullPointerException();//throws exception if both stacks are empty
-        while(inputStack != null){
-            outputStack.push(inputStack.pop());
+    public int dequeue() throws NullPointerException {
+        try {
+            return outputStack.pop();//pops outputStack if not empty
+        } catch (NullPointerException e) {
+            loadStack();//loads outputStack if it is empty
+            return outputStack.pop();
         }
-        return outputStack.pop();
+
+    }
+
+    public void loadStack() throws NullPointerException {
+        try {
+            while (inputStack != null) {
+                outputStack.push(inputStack.pop());//this pops of the top of the input stack into the output stack, effectively turning the outputStack into the queue of what has been put in the inputStack
+            }
+        } catch (NullPointerException e) {//returns exception if inputStack is empty
+
+        }
     }
 }
